b0c07601a837918b03e4916fc2cb6c164870cf21
AdriKat2022 <adrien.schroedel91@gmail.com>
2024-07-28 17:01:30 +0200

    created core js code

--- js/scripts.js
+++ js/scripts.js
@@ -24,19 +24,21 @@ const translations = {
         "title-about": "À propos",
         "title-contact": "Contact",
         "profile-subtitle": "Vous avez un projet en tête dans le domaine du game dev ou du software ?<br>N'hésitez pas à me contacter par mail ou par téléphone !",
+        
         "profile-column-1-head": "Maîtrise de multiples <strong>languages de programmation</strong> parmi...",
-            "profile-column-1-1": "C++ (avec notamment <em>Unreal Engine 5</em>)",
-            "profile-column-1-2": "C# (avec <em>Unity</em>)",
-            "profile-column-1-3": "C",
-            "profile-column-1-4": "Python",
-            "profile-column-1-5": "Java",
-            "profile-column-1-6": "Bash (dans un environnement <em>Linux</em>)",
-            "profile-column-1-7": "HTML/CSS/PHP/JavaScript (avec Symphony pour le web)",
+        "profile-column-1-1": "C++ (avec notamment <em>Unreal Engine 5</em>)",
+        "profile-column-1-2": "C# (avec <em>Unity</em>)",
+        "profile-column-1-3": "C",
+        "profile-column-1-4": "Python",
+        "profile-column-1-5": "Java",
+        "profile-column-1-6": "Bash (dans un environnement <em>Linux</em>)",
+        "profile-column-1-7": "HTML/CSS/PHP/JavaScript (avec Symphony pour le web)",
+        
         "profile-column-2-head": "Maîtrise de multiples <strong>outils</strong> de gestion de projet et burautique parmi...",
-            "profile-column-2-1": "Gestionnaire de version GIT (+ LFS)",
-            "profile-column-2-2": "Suite Microsoft Office",
-            "profile-column-2-3": "Suite Google",
-            "profile-column-2-4": "Microsoft Office",
+        "profile-column-2-1": "Gestionnaire de version GIT (+ LFS)",
+        "profile-column-2-2": "Suite Microsoft Office",
+        "profile-column-2-3": "Suite Google",
+        "profile-column-2-4": "Microsoft Office",
         
         "about-p1": "Bonjour et bienvenue sur mon portfolio ! Je m'appelle Adrien Schroedel, passionné par le software et le game dev, j'ai tout naturellement intégré une école (Télécom SudParis) pour devenir ingénieur du numérique...<br/>Tout au long de ma formation, j'ai participé à de nombreux <a href=\"#portfolio\">projets de création de jeux vidéo</a>.",
         "about-p2": "Mon plaisir vient non seulement d'apporter mon expertise sur les moteurs de jeu les plus utilisés sur le marché, mais aussi de collaborer harmonieusement avec une équipe pluridisciplinaire pour offrir une œuvre digne du 10me art à tout type de publics.",
@@ -49,20 +51,16 @@ const translations = {
 
         "projects-button-close":"<i class=\"fas fa-xmark fa-fw\"></i>Fermer",
 
-        "project-6-title": "Rupture",
-            "project-6-date": "Janvier - Juin 2024",
-            "project-6-subtitle": "Un jeu vidéo impactant sur le thème du Développement Durable",
-            "project-6-description-1": "<strong>Rupture</strong> est un projet réalisé par un groupe de 4 étudiants dans le cadre d'une formation en tant qu'ingénieur du numérique à Télécom SudParis.",
-            "project-6-description-2": "Dans ce jeu à style narratif, vous incarnez le point de vue d'un humble robot constatant le statut de la société se plaçant dans un futur proche. Alors que votre simple mission est de rendre service à un jeune couple, votre tâche se métamorphose alors de manière imprévue, en une bien plus importante qu'un simple divertissement.",
-            "project-6-description-3": "L'objectif primaire de ce projet était notamment d'apprendre à utiliser le moteur UnrealEngine 5 pour créer un jeu vidéo impactant sur le thème du Développement Durable.",
-            "project-6-description-4": "Le rapport de notre projet est disponible.",
 
-            "project-6-button-download": "<i class=\"fas fa-download me-2\"></i>Télécharger le rapport",
+        // Rupture project
+        "project-rupture-title": "Rupture",
+        "project-rupture-date": "Janvier - Juin 2024",
+        "project-rupture-subtitle": "Un jeu vidéo impactant sur le thème du Développement Durable",
+        "project-rupture-description-1": "<p><strong>Rupture</strong> est un projet réalisé par un groupe de 4 étudiants dans le cadre d'une formation d'ingénieur du numérique à Télécom SudParis.</p>",
+        "project-rupture-description-2": "<p>Dans ce jeu de type narratif, vous incarnez le point de vue d'un modeste robot observant l'état de la société dans un futur proche. Alors que votre mission simple est d'aider un jeune couple, votre tâche se métamorphose alors de manière inattendue en une mission bien plus importante que de simple divertissement.</p><p>L'objectif principal de ce projet était notamment d'apprendre à utiliser le moteur UnrealEngine 5 pour créer un jeu vidéo impactant sur le thème du Développement Durable.</p><p>Le rapport de notre projet est disponible.</p>",
+        "project-rupture-button-download": "<i class=\"fas fa-download me-2\"></i>Télécharger le rapport",
     },
-
-
-
-
+    /////////////////////////////////////////////////////////////////////////
     en: {
         "job-title": "@AdriKat - Developer",
         "job-subtitle": "Digital engineer - Game developer and designer - Software developer",
@@ -75,19 +73,21 @@ const translations = {
         "title-about": "About",
         "title-contact": "Contact",
         "profile-subtitle": "Have a project in mind in the field of game or software dev ?<br>Do not hesitate to contact me by phone or email !",
+        
         "profile-column-1-head": "Mastery of multiple <strong>programming languages</strong> including...",
-            "profile-column-1-1": "C++ (including <em>Unreal Engine 5</em>)",
-            "profile-column-1-2": "C# (with <em>Unity</em>)",
-            "profile-column-1-3": "C",
-            "profile-column-1-4": "Python",
-            "profile-column-1-5": "Java",
-            "profile-column-1-6": "Bash (in a <em>Linux</em> environment)",
-            "profile-column-1-7": "HTML/CSS/PHP/JavaScript (with Symphony for the web)",
+        "profile-column-1-1": "C++ (including <em>Unreal Engine 5</em>)",
+        "profile-column-1-2": "C# (with <em>Unity</em>)",
+        "profile-column-1-3": "C",
+        "profile-column-1-4": "Python",
+        "profile-column-1-5": "Java",
+        "profile-column-1-6": "Bash (in a <em>Linux</em> environment)",
+        "profile-column-1-7": "HTML/CSS/PHP/JavaScript (with Symphony for the web)",
+
         "profile-column-2-head": "Mastery of multiple <strong>project management and office tools</strong> including...",
-            "profile-column-2-1": "GIT version manager (+ LFS)",
-            "profile-column-2-2": "Microsoft Office Suite",
-            "profile-column-2-3": "Google Suite",
-            "profile-column-2-4": "Microsoft Office",
+        "profile-column-2-1": "GIT version manager (+ LFS)",
+        "profile-column-2-2": "Microsoft Office Suite",
+        "profile-column-2-3": "Google Suite",
+        "profile-column-2-4": "Microsoft Office",
         
         "about-p1": "Hello and welcome to my portfolio ! My name is Adrien Schroedel, passionate about software and game dev, I naturally joined a school (Télécom SudParis) to become a digital engineer...<br/>Throughout my training, I participated in many <a href=\"#portfolio\">video game creation projects</a>.",
         "about-p2": "My pleasure comes not only from bringing my expertise on the most used game engines on the market, but also from harmoniously collaborating with a multidisciplinary team to offer a work worthy of the 10th art to all types of audiences.",
@@ -100,18 +100,103 @@ const translations = {
 
         "projects-button-close":"<i class=\"fas fa-xmark fa-fw\"></i>Close",
 
-        "project-6-title": "Rupture",
-            "project-6-date": "January - June 2024",
-            "project-6-subtitle": "An impactful video game on the theme of Sustainable Development",
-            "project-6-description-1": "<strong>Rupture</strong> is a project carried out by a group of 4 students as part of a training as a digital engineer at Télécom SudParis.",
-            "project-6-description-2": "In this narrative-style game, you embody the point of view of a humble robot observing the status of society in the near future. While your simple mission is to help a young couple, your task then unexpectedly metamorphoses into a much more important one than mere entertainment.",
-            "project-6-description-3": "The primary objective of this project was notably to learn how to use the UnrealEngine 5 engine to create an impactful video game on the theme of Sustainable Development.",
-            "project-6-description-4": "The report of our project is available.",
-
-            "project-6-button-download": "<i class=\"fas fa-download me-2\"></i>Download the report",
+        // Rupture project
+        "project-rupture-title": "Rupture",
+        "project-rupture-date": "January - June 2024",
+        "project-rupture-subtitle": "An impactful video game on the theme of Sustainable Development",
+        "project-rupture-description-1": "<p><strong>Rupture</strong> is a project carried out by a group of 4 students as part of a training as a digital engineer at Télécom SudParis.</p>",
+        "project-rupture-description-2": "<p>In this narrative-style game, you embody the point of view of a humble robot observing the status of society in the near future. While your simple mission is to help a young couple, your task then unexpectedly metamorphoses into a much more important one than mere entertainment.</p><p>The primary objective of this project was notably to learn how to use the UnrealEngine 5 engine to create an impactful video game on the theme of Sustainable Development.</p><p>The report of our project is available.</p>",
+        "project-rupture-button-download": "<i class=\"fas fa-download me-2\"></i>Download the report",
     }
 }
 
+
+// Project loader
+const projects = [
+    {
+        id: 'rupture',
+        cover_img: 'assets/img/portfolio/rupture.jpg',
+        imgs: ['assets/img/portfolio/rupture.jpg'],
+        // For example download the report
+        action: {type: 'download', link: 'assets/docs/rupture-report.pdf'}
+    },
+
+
+];
+
+
+const loadProjects = () => {
+    
+    // Cover cards
+    const portfolioContainer = document.getElementById('portfolio-grid-container');
+    const cardTemplate = document.getElementById('portfolio-card-template');
+    // Modals popups
+    const portfolioModals = document.getElementById('portfolio-modals');
+    const modalTemplate = document.getElementById('portfolio-modal-template');
+
+    projects.forEach((project, index) => {
+        // Clone and populate the cover card template
+        const card = cardTemplate.content.cloneNode(true);
+        const cardElement = card.querySelector('.portfolio-item');
+        cardElement.id = `portfolio-item-${project.id}`;
+        cardElement.setAttribute('data-bs-target', '#portfolio-modal-' + project.id);
+        const cardImg = cardElement.querySelector('.portfolio-image');
+        cardImg.src = project.cover_img;
+
+        // Add the card to the portfolio container
+        portfolioContainer.appendChild(card);
+
+
+        // Clone and populate the modal template
+        const modal = modalTemplate.content.cloneNode(true);
+        const modalElement = modal.querySelector('.portfolio-modal');
+        modalElement.id = `portfolio-modal-${project.id}`;
+        modalElement.setAttribute('aria-labelledby', 'portfolio-modal-' + project.id);
+
+        // Get the text elements with their ids
+        // get the title element (id="project-title")
+        const titleElement = modalElement.querySelector('#project-title');
+        titleElement.innerHTML = translations[DEFAULT_LANGUAGE][`project-${project.id}-title`];
+        // get the date element (id="project-date")
+        const dateElement = modalElement.querySelector('#project-date');
+        dateElement.innerHTML = translations[DEFAULT_LANGUAGE][`project-${project.id}-date`];
+        // get the subtitle element (id="project-subtitle")
+        const subtitleElement = modalElement.querySelector('#project-subtitle');
+        subtitleElement.innerHTML = translations[DEFAULT_LANGUAGE][`project-${project.id}-subtitle`];
+        // get the description element (id="project-description-1")
+        const descriptionElement1 = modalElement.querySelector('#project-description-1');
+        descriptionElement1.innerHTML = translations[DEFAULT_LANGUAGE][`project-${project.id}-description-1`];
+        // get the description element (id="project-description-2")
+        const descriptionElement2 = modalElement.querySelector('#project-description-2');
+        descriptionElement2.innerHTML = translations[DEFAULT_LANGUAGE][`project-${project.id}-description-2`];
+        // get the button element (id="project-button-download")
+        const buttonElement = modalElement.querySelector('#project-button-download');
+        buttonElement.innerHTML = translations[DEFAULT_LANGUAGE][`project-${project.id}-button-download`];
+
+        // Add the images to the modal
+        const modalImages = modalElement.querySelector('.portfolio-modal-carousel');
+        project.imgs.forEach((img, index) => {
+            const carouselItem = document.createElement('div');
+            carouselItem.classList.add('carousel-item');
+            if (index === 0) {
+                carouselItem.classList.add('active');
+            }
+            const imgElement = document.createElement('img');
+            imgElement.src = img;
+            imgElement.classList.add('d-block', 'w-100');
+            carouselItem.appendChild(imgElement);
+            modalImages.appendChild(carouselItem);
+        });
+
+        // Add the modal to the modals container
+        portfolioModals.appendChild(modal);
+    });
+
+    };
+
+
+
+// Language selector
 const languageSelector = document.getElementById(LANGUAGE_SELECTOR_ID);
 
 languageSelector.addEventListener('change', (event) => {
